149c5b04c295822fd2b1ba7399b66845301cec69
Knute Snortum <knute@snortum.net>
2022-12-12 15:17:19 -0800

    Minor cleanup

--- src/calculator/Main.kt
+++ src/calculator/Main.kt
@@ -6,15 +6,14 @@ private const val INVALID_ASSIGNMENT = "Invalid assignment"
 private const val UNKNOWN_VARIABLE = "Unknown variable"
 
 private val intRegex = "^([+-]?\\d+)".toRegex()
-private val opRegex = "^(\\++|-+|\\*|/)".toRegex()
+private val opRegex = "^(\\++|-+|\\*|/)".toRegex()  // Tied to enum Operator
 private val identRegex = "^([a-zA-Z]+)".toRegex()
 
-private val vars = emptyMap<String, Int>().toMutableMap()
+private val vars = mutableMapOf<String, Int>()
 
 class Calculator {
-    data class IntRecord(val value: Int = 0, val isError: Boolean = false, val errorMessage: String = "")
-    data class StringRecord(val value: String = "", val isError: Boolean = false, val errorMessage: String = "")
 
+    // See also: checkOperator() and applyOperator()
     enum class Operator(val symbol: String) {
         PLUS("+"),
         MINUS("-"),
@@ -39,12 +38,17 @@ class Calculator {
                     else -> println("Unknown Command")
                 }
             } else {
-                // TODO, use throw exception instead of intRecord and stringRecord
-                // Might be an assignment
-                if (input.contains('=')) {
-                    doAssignment()
-                } else {
-                    println(evaluateExpression())
+                try {
+                    // Might be an assignment
+                    if (input.contains('=')) {
+                        doAssignment()
+                    } else {
+                        // Evaluate expression
+                        println(evaluateExpression())
+                    }
+                // All errors from doAssignment() and evaluateExpression() (and below) bubble up to here
+                } catch (e: Exception) {
+                    println(e.message)
                 }
             }
         }
@@ -52,130 +56,127 @@ class Calculator {
         println("Bye!")
     }
 
+    /**
+     * Assignment in the form identifier1 = (intValue | identifier2)
+     */
     private fun doAssignment() {
-        // TODO, use split \\s*=\\s ?
         // Get left-hand side of assignment
-        val stringRecord = getIdent()
-        if (stringRecord.isError) {
-            println(stringRecord.errorMessage)
-            return
-        }
-        val lhs = stringRecord.value
+        val lhs = getIdent()
 
         // Get assignment operator
-        removeWhitespace()
+        input = input.trimStart()
         // if (input.isEmpty()) ...?
-        if (input[0] != '=') {
-            println(INVALID_IDENTIFIER)
-            return
-        }
-        input = input.substring(1)
-
-        removeWhitespace()
-        if (input.isEmpty()) {
-            println(INVALID_ASSIGNMENT)
-            return
-        }
+        if (input.first() != '=') throw Exception(INVALID_IDENTIFIER)
+        input = input.drop(1).trimStart()
+        if (input.isEmpty()) throw Exception(INVALID_ASSIGNMENT)
 
         // Get right-hand side of assignment
+        // TODO, rhs should really be able to handle a full expression
         val rhs: Int
         val matchResult = intRegex.find(input)
-        if (matchResult == null) {
-            val strRecord = getIdent()
-            if (strRecord.isError) {
-                println(INVALID_ASSIGNMENT)
-                return
-            }
-            val ident = strRecord.value
 
-            val intRecord = getVariable(ident)
-            if (intRecord.isError) {
-                println(intRecord.errorMessage)
-                return
+        // Not an integer
+        if (matchResult == null) {
+            val ident: String
+            // An invalid identifier here is considered an invalid assignment for some reason.
+            try {
+                ident = getIdent()
+            } catch (e: Exception) {
+                throw Exception(INVALID_ASSIGNMENT)
             }
-            rhs = intRecord.value
+            rhs = getVariable(ident)
         } else {
+            // Is an integer
             input = input.replace(intRegex, "")
             rhs = matchResult.value.toInt()
         }
 
+        // Nothing can come after the assignment
         if (input.isBlank()) {
             vars[lhs] = rhs
         } else {
-            println(INVALID_ASSIGNMENT)
+            throw Exception(INVALID_ASSIGNMENT)
         }
     }
 
-    private fun getIdent(): StringRecord {
-        val matchResult =
-            identRegex.find(input) ?: return StringRecord(isError = true, errorMessage = INVALID_IDENTIFIER)
+    /**
+     * Get an identifier.  Then it looks ahead for certain conditions before returning
+     */
+    private fun getIdent(): String {
+        val matchResult = identRegex.find(input) ?: throw Exception(INVALID_IDENTIFIER)
         input = input.replace(identRegex, "")
 
-        if (input.isBlank() ||
-            input[0].isWhitespace() ||
-            input.trimEnd()[0] == '=' ||
-            allOperators.contains(input.trimEnd()[0])
+        // These are the only things that can follow a valid identifier
+        if (input.isBlank() || // end of line
+            input.first().isWhitespace() || // a whitespace character
+            input.trimEnd().first() == '=' || // an equals sign
+            allOperators.contains(input.trimEnd().first()) // or any valid operator
         ) {
-            return StringRecord(matchResult.value)
+            return matchResult.value
         }
 
-        return StringRecord(isError = true, errorMessage = INVALID_IDENTIFIER)
+        throw Exception(INVALID_IDENTIFIER)
     }
 
-    private fun getVariable(ident: String): IntRecord {
+    /**
+     * @return the integer amount for this variable, or throws error
+     */
+    private fun getVariable(ident: String): Int {
         return if (vars.contains(ident)) {
-            IntRecord(vars[ident]!!)
+            vars[ident]!!
         } else {
-            IntRecord(isError = true, errorMessage = UNKNOWN_VARIABLE)
+            throw Exception(UNKNOWN_VARIABLE)
         }
     }
 
-    private fun evaluateExpression(): String {
+    /**
+     * Expression is in the form (intValue | variable) [operator (intValue | variable) [...]]
+     * @return the result of evaluating the expression
+     */
+    private fun evaluateExpression(): Int {
+        // First applyOperator() will add zero, that is, do nothing
         var result = 0
         var operator = Operator.PLUS
 
         while (true) {
-            val record = getInteger()
-            if (record.isError) return record.errorMessage
-            result = applyOperator(operator, record.value, result)
+            val number = getInteger()
+            result = applyOperator(operator, number, result)
 
             if (input.isBlank()) break
 
             operator = getOperator()
-            if (operator == Operator.UNKNOWN) return INVALID_EXPRESSION
+            if (operator == Operator.UNKNOWN) throw Exception(INVALID_EXPRESSION)
         }
 
-        return result.toString()
+        return result
     }
 
     /**
      * Get an integer or a variable value from the input string.
-     * @return IntRecord with the value set, or isError set to true with an error message
      */
-    private fun getInteger(): IntRecord {
-        removeWhitespace()
-        if (input.isEmpty()) return IntRecord(isError = true, errorMessage = INVALID_EXPRESSION)
+    private fun getInteger(): Int {
+        input = input.trimStart()
+        if (input.isEmpty()) throw Exception(INVALID_EXPRESSION)
         val matchResults = intRegex.find(input)
+        val result: Int
 
+        // If you don't get an integer value from the regex, try a variable
         if (matchResults == null) {
-            val rec = getIdent()
-            if (rec.isError) return IntRecord(isError = true, errorMessage = INVALID_IDENTIFIER)
-            val record = getVariable(rec.value)
-            if (!record.isError) input = input.replace(identRegex, "")
-            return record
+            result = getVariable(getIdent())
+            input = input.replace(identRegex, "")
         } else {
-            val result = matchResults.value.toInt()
+            result = matchResults.value.toInt()
             input = input.replace(intRegex, "")
-            return IntRecord(result)
         }
+
+        return result
     }
 
-    private fun removeWhitespace() {
-        input = input.replace("^\\s*".toRegex(), "")
-    }
-
+    /**
+     * Get a valid operator
+     */
     private fun getOperator(): Operator {
-        removeWhitespace()
+        input = input.trimStart()
         val matchResults = opRegex.find(input) ?: return Operator.UNKNOWN
         val result = matchResults.value
         input = input.replace(opRegex, "")
@@ -183,10 +184,13 @@ class Calculator {
         return checkOperator(result)
     }
 
+    /**
+     * Return an operator, possibly UNKNOWN.  Takes care or multiple '+' or '-'.
+     */
     private fun checkOperator(operator: String): Operator {
-        if (operator.matches("""(\+)(\1)*""".toRegex())) {
+        if (operator.matches("\\++".toRegex())) {
             return Operator.PLUS
-        } else if (operator.matches("""(-)(\1)*""".toRegex())) {
+        } else if (operator.matches("-+".toRegex())) {
             return if (operator.length % 2 == 0) Operator.PLUS else Operator.MINUS
         }
 
@@ -199,6 +203,9 @@ class Calculator {
         }
     }
 
+    /**
+     * Apply this operator and number to the result
+     */
     private fun applyOperator(operator: Operator, number: Int, result: Int): Int {
         return when (operator) {
             Operator.PLUS -> result + number
